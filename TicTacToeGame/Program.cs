089df622568fbc379d345844b8427a2dfcb401f0
santanu619 <mohapatra.santanu123@gmail.com>
2021-05-07 11:43:39 +0530

    Added: Initial code for empty first slot

---
+++ TicTacToeGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToeGame game = new TicTacToeGame();
+            game.GameBoard();
+        }
+    }
+}
---
+++ TicTacToeGame/TicTacToeGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGame
+{
+    class TicTacToeGame
+    {
+        public void GameBoard()
+        {
+            char[] board = new char[10];
+            for(int i=1;i<=10;i++)
+            {
+                board[1] = ' ';
+            }
+        }
+    }
+}
